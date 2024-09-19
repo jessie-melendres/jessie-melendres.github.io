aa8669beae77c945d4f62a8d9a23580c241f595a
jessie-melendres <58218701+jessie-melendres@users.noreply.github.com>
2024-09-19 04:48:14 +0000

    Add other configurations

---
+++ portfolio/docs/portfolio/server/chunk-RNHHWYUA.mjs
@@ -0,0 +1,3 @@
+import './polyfills.server.mjs';
+import{a as p,b as r,c as n,d as i,e as m,j as a,m as f,o as s,p as c,s as l,y as d}from"./chunk-WSS7KKBA.mjs";var o=class e{title="portfolio";static \u0275fac=function(t){return new(t||e)};static \u0275cmp=p({type:e,selectors:[["app-root"]],standalone:!0,features:[m],decls:2,vars:0,consts:[[1,"header"]],template:function(t,R){t&1&&(r(0,"h1",0),i(1,` HELLO WORLD!
+`),n())}})};var u=[];var C={providers:[a({eventCoalescing:!0}),d(u),c()]};var A={providers:[l()]},g=f(C,A);var x=()=>s(o,g),j=x;export{j as a};
---
+++ portfolio/docs/portfolio/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-RNHHWYUA.mjs";import"./chunk-WSS7KKBA.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
